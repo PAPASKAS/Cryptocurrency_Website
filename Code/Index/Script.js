44cd8422bc64e9c4e322d0888820554bd48f91a8
Pavel <Papaskaaaa@gmail.com>
2021-05-14 02:10:37 +0300

    % - calculator / crypto / features / start mining

--- Code/Index/Script.js
+++ Code/Index/Script.js
@@ -47,3 +47,59 @@ document.onclick = function() {
         },1)
     }
 }
+//calculator
+
+function select_hash(){
+    let height_select_hash = document.querySelector(".option_calculator").offsetHeight
+    let open_var = 0
+    let closed_var = 104
+    
+    if(height_select_hash === 0){
+        document.querySelector(".option_calculator").style.display = "block"
+        document.querySelector(".option_calculator").style.height ="0px"    
+        document.querySelector(".select_calculator img").style.transform = "rotate(180deg)"
+            
+        
+        let open_anima = setInterval(function(){
+            if(open_var < 92){
+                open_var+=13
+                document.querySelector(".option_calculator").style.height = open_var + "px"
+            }else{
+                clearInterval(open_anima)
+            }
+        },10)    
+    }else{
+        document.querySelector(".select_calculator img").style.transform = "rotate(360deg)"
+        let closed_anima = setInterval(function(){
+            if(closed_var > 0){
+                closed_var-=13
+                document.querySelector(".option_calculator").style.height = closed_var + "px"
+            }else{
+                document.querySelector(".option_calculator").style.display = ""
+                clearInterval(closed_anima)
+            }
+        },10)    
+    }
+}
+
+function calculator(){
+    
+}
+
+function crypto_over(n){
+    document.querySelectorAll(".crypto_item")[n].style.backgroundColor = "#2B076E"
+    document.querySelectorAll("#name_crypto")[n].style.color = "#FFFFFF"
+    document.querySelectorAll("#abbreviated_name")[n].style.color = "#FFFFFF"
+    document.querySelectorAll("#description_crypto")[n].style.color = "#FFFFFF"
+    document.querySelectorAll("#button_crypto")[n].src = "../../Source/Img/Button_crypto.svg"
+    document.querySelectorAll(".shadow_crypto span")[n].style.boxShadow = "none"
+}
+
+function crypto_out(n){
+    document.querySelectorAll(".crypto_item")[n].style.backgroundColor = "#F8F9FB"
+    document.querySelectorAll("#name_crypto")[n].style.color = "#0D0D2B"
+    document.querySelectorAll("#abbreviated_name")[n].style.color = "#BDBDBD"
+    document.querySelectorAll("#description_crypto")[n].style.color = "#828282"
+    document.querySelectorAll("#button_crypto")[n].src = "../../Source/Img/Arrow Right.svg"
+    document.querySelectorAll(".shadow_crypto span")[n].style.boxShadow = "0px 40px 40px #391777"
+}
